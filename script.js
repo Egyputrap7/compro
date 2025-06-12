b7acfdfbe052f52e693a45955add24cd6a257ef2
Egyputra7 <egyputra78@gmail.com>
2025-06-12 12:49:22 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const sections = document.querySelectorAll('.section');
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = 1;
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
+
+sections.forEach(section => {
+  observer.observe(section);
+});
